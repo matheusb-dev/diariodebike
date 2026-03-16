09d9857a519073febc94cc638ade5f7e30434642
matheusb-dev <leitinhogames6@gmail.com>
2026-03-16 19:21:52 -0300

    Routes :D

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 //Router
 router.get("", (req, res) => {
-  res.send("Hello world");
+  const locals = {
+    title: "diariodebike",
+    description:
+      "Diario de Bike em Nova Esperança - PR, post quando eu conseguir pedalar kskksk.",
+  };
+
+  res.render("index", { locals });
+});
+
+//Router
+router.get("/about", (req, res) => {
+  const locals = {
+    title: "diariodebike",
+    description:
+      "Diario de Bike em Nova Esperança - PR, post quando eu conseguir pedalar kskksk.",
+  };
+  res.render("about", { locals });
 });
 
 module.exports = router;
